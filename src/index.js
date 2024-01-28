f4b59cf840572d6e6774078e70d8a356af841ea8
jadejourney <jadejourney99@gmail.com>
2024-01-28 07:31:05 -0500

    Added test case

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 import * as Sentry from '@sentry/browser';
 
+// Testing application comment
+
 Sentry.init({dsn: "https://0632561d7d784893878cd1234ca9b1e8@o395744.ingest.sentry.io/5248132"});
 
 ReactDOM.render(
